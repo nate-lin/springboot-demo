692815326ec8f821a3fb4fa40d180692eb4a7fc6
nate-lin <linchunyan335@163.com>
2020-08-13 17:44:21 +0800

    修改

--- springboot-02-mybatis/src/main/java/qian/yue/Springboot02MybatisApplication.java
+++ springboot-02-mybatis/src/main/java/qian/yue/Springboot02MybatisApplication.java
@@ -1,9 +1,11 @@
 package qian.yue;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("qian.yue.mapper")
 public class Springboot02MybatisApplication {
 
 	public static void main(String[] args) {
--- springboot-02-mybatis/src/main/java/qian/yue/mapper/UserMapper.java
+++ springboot-02-mybatis/src/main/java/qian/yue/mapper/UserMapper.java
@@ -1,4 +1,23 @@
 package qian.yue.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import qian.yue.pojo.User;
+
+import java.util.List;
+
+//这个注解表示这是一个mybatis的mapper类
+@Mapper
+@Repository
 public interface UserMapper {
+
+    List<User> queryUserList();
+
+    User queryUserById(int id);
+
+    int addUser(User user);
+
+    int updateUser(User user);
+
+    int deleteUser(int id);
 }
